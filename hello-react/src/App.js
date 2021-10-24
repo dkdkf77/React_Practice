9a34b5c33b2e70393c1b522f0c21847574e173b7
SEUNGHWAN <88579497+dkdkf77@users.noreply.github.com>
2021-10-24 17:52:32 +0900

    1. 리액트 propsTypes를 배워보았다.

---
+++ hello-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import MyComponent from './MyComponent';
+
+const App = () => {
+  return (
+    <MyComponent name="JYP" favoriteNumber={34}>
+      류띵똥
+    </MyComponent>
+  );
+};
+
+export default App;
