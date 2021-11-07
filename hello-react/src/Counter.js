9f730f5aca819401597d9d2b8eea2fb0ec174a7a
SEUNGHWAN <88579497+dkdkf77@users.noreply.github.com>
2021-11-07 16:22:40 +0900

    this.setState에 관하여 공부

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    //super = props에서 상속 받아 온다는 뜻으로 배움
-    super(props);
-    //state의 초깃값 설정하기
-    this.state = {
-      number: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
   render() {
-    const { number } = this.state; // state를 조회할 때는 this.state로 조회
+    const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값 : {fixedNumber}</h2>
         <button
           // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정
           onClick={() => {
-            // this.setState 를 사용하여 state에 새로운 값을 넣음.
-            this.setState({ number: number + 1 });
+            this.setState(
+              {
+                number: number + 1,
+              },
+              () => {
+                console.log('방금 setState가 호출되었습니다.');
+                console.log(this.state);
+              }
+            );
           }}
         >
           +1
